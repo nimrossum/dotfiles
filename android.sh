a579050ba4f6552932f2e9605f8f9af02a0f070d
nimrossum <1959615+nimrossum@users.noreply.github.com>
2026-03-04 14:46:24 +0100

    Add NVM installation to android.sh script

--- android.sh
+++ android.sh
@@ -1,4 +1,6 @@
 sudo apt update
 sudo apt install unzip
 curl -fsSL https://bun.sh/install | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source /home/droid/.bashrc
+nvm install latest
